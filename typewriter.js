406864914e4431d10a76f2b0bf635b12bf53351d
Sean Jin <seanhoyeonjin@gmail.com>
2021-09-21 18:12:17 +0000

    some changes to the typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,13 @@
 const sentence = 'hello there from lighthouse labs';
-delay = 0;
+let delay = 0;
+
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
   }, delay);
   delay += 50;
 }
+
 setTimeout(() => {
   console.log("");
 }, delay)
